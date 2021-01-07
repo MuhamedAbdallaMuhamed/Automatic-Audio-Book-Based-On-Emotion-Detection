7b4fd68cc4e45de220cc365659f8e8044e4da596
Nader <nader.walid@mobidevlabs.com>
2021-01-07 13:29:50 +0200

    add lib file

---
+++ Front-End/EmotionSpeaker/lib/constants/custom_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const Color backgroundColor = Color(0xffFFF3E7);
+  static const Color color3 = Color(0xff94CCE7);
+  static const Color color2 = Color(0xffDC6068);
+  static const Color color1 = Color(0xffEEBC5A);
+}
