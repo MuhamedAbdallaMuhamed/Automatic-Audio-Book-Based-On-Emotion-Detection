be21c5a1d5dbbc0dbf36af9e44cf3f30284b38db
ahmedShakshak <ahmedshakshak.fcis@gmail.com>
2021-01-25 22:17:05 +0200

    solved relative paths issue

--- backend/microservices/auth/__init__.py
+++ backend/microservices/auth/__init__.py
@@ -1,4 +1,7 @@
-from .controller import app
+import sys
+from os import path
+sys.path.append(path.join(path.realpath(__file__), '..\..'))
+from auth.controller import app
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
