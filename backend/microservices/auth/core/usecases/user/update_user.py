7364a8bab9c86be897af485532ad3487a1d62fbc
Ahmed Amin <ahmed@192.168.1.7>
2021-01-29 18:22:20 +0200

    made update user handel passed old data

--- backend/microservices/auth/core/usecases/user/update_user.py
+++ backend/microservices/auth/core/usecases/user/update_user.py
@@ -17,21 +17,21 @@ def build_update_user(db_update_user, add_image_to_storage):
             add_image_to_storage(old_user.id, profile_picture_data)
         edited_user = edit_user(
                         id=old_user.id,
-                        first_name=first_name if first_name is not None else old_user.first_name,
-                        last_name=last_name if last_name is not None else old_user.last_name,
-                        email=email if email is not None else old_user.email,
+                        first_name=first_name if first_name != old_user.first_name else old_user.first_name,
+                        last_name=last_name if last_name != old_user.last_name else old_user.last_name,
+                        email=email if email != old_user.email else old_user.email,
                         password=password if password is not None else old_user.hashed_password,
                         salt=old_user.salt,
-                        phone=phone if phone is not None else old_user.phone,
-                        birthday=birthday if birthday is not None else old_user.birthday,
-                        gender=gender if gender is not None else old_user.gender,
-                        new_email=email is not None,
-                        new_phone=phone is not None,
-                        new_gender=gender is not None,
-                        new_birthday=birthday is not None,
+                        phone=phone if phone != old_user.phone else old_user.phone,
+                        birthday=birthday if birthday != old_user.birthday else old_user.birthday,
+                        gender=gender if gender != old_user.gender else old_user.gender,
+                        new_email=email != old_user.email,
+                        new_phone=phone != old_user.phone,
+                        new_gender=gender != old_user.gender,
+                        new_birthday=birthday != old_user.birthday,
                         new_password=password is not None,
-                        new_last_name=last_name is not None,
-                        new_first_name=first_name is not None,
+                        new_last_name=last_name != old_user.last_name,
+                        new_first_name=first_name != old_user.first_name,
                     )
         return db_update_user(edited_user)
     return update_user
