a8198417a491e43cea6e22102180aa5cf702b967
MuhamedAbdalla <medokingdom7@outlook.com>
2021-05-03 02:44:42 +0200

    Req format

--- backend/microservices/TTS/main.py
+++ backend/microservices/TTS/main.py
@@ -70,4 +70,4 @@ def TTS():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=6060)
+    app.run(host='0.0.0.0', debug=True, port=8000)
--- backend/microservices/audio-generator/__init__.py
+++ backend/microservices/audio-generator/__init__.py
@@ -4,4 +4,4 @@ sys.path.append(path.join(path.realpath(__file__), '..'))
 from controller import app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=5050)
+    app.run(host='0.0.0.0', debug=True, port=7000)
